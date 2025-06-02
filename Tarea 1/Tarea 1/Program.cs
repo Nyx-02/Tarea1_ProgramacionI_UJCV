779e9f9f95d2a6521d381e7f18f32e0ad4c3c9fd
Nyxby2002 <nyxby2002@gmail.com>
2025-06-01 18:29:55 -0600

    Creé el proyecto en C# y agregué código inicial.

---
+++ Tarea 1/Tarea 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hola, Mundo");
+        Console.WriteLine("Este es un codigo de prueba para la visualizacion de clonado de un repositorio de github");
+        Console.WriteLine("Creado por: Bryan Galo");
+    }
+}
