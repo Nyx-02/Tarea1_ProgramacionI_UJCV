936d90b42d689c4ec562f44adbdd7c1dfed36f00
Nyxby2002 <nyxby2002@gmail.com>
2025-06-01 18:48:00 -0600

    rama de ejemplo

--- Tarea 1/Tarea 1/Program.cs
+++ Tarea 1/Tarea 1/Program.cs
@@ -7,5 +7,7 @@ class Program
         Console.WriteLine("Hola, Mundo");
         Console.WriteLine("Este es un codigo de prueba para la visualizacion de clonado de un repositorio de github");
         Console.WriteLine("Creado por: Bryan Galo");
+
+        Console.Witeline("Este es un texto que solo se puede ver en la rama de ejemplo");
     }
 }
